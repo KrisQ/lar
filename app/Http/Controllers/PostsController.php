6ff6b0efc4201d4fc9d0a1aa61b7971e5b575597
KrisQ <kquemener@yahoo.com>
2018-02-26 08:14:13 -0600

    first

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return "<h4>I am the create method biatch</h4>";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return "This is the show method $id";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function contact(){
+      return view('contact');
+    }
+
+    public function show_post($id){
+      //return view('post')->with('id',$id);
+      return view('post', compact('id'));
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::resource('posts', 'PostsController');
+//Route::get('contact', 'PostsController@contact');
+
+Route::get('post/{id}', 'PostsController@show_post');
+
+// Route::get('/post/{id}', 'PostsController@index');
+
+// Route::get('/about', function(){
+//   return '<a href="'.route('admin.home').'">Yooo</a>';
+// });
+//
+// Route::get('/contact', function(){
+//   return "<p>contact page</p>";
+// });
+//
+// Route::get('/post/{id}/{name}', function($id, $name){
+//   return "<h1>Post #$id</h1> $name";
+// });
+//
+// Route::get('admin/posts/example', array(
+//   'as'=>'admin.home' ,
+//    function(){
+//      $url = route('admin.home');
+//      return "this url is $url";
+// }));
